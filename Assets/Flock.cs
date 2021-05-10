2da082990748a1c1f3cecd30a04131d6a861cf8a
giovanni de camargo <giosilg@gmail.com>
2021-05-10 19:11:54 -0300

    upgrade
    upgrade

--- Assets/Flock.cs
+++ Assets/Flock.cs
@@ -4,33 +4,57 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-    public FlockManager myManager;
-    public float speed;
-    bool turing = false;
+    public FlockManager myManager;//conexao com flockmanager.
+    public float speed;//varia
+    bool turing = false;//variavel de ativação do turning
     void Start()
     {
-        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+        //diretiva do controle de velocidade
+        speed = Random.Range(myManager.minSpeed,
+            myManager.maxSpeed);
 
     }
 
     void Update()
     {
+        //limitador de espaço para nao deixar os peixes pederem referencia entre si
         Bounds b = new Bounds(myManager.transform.position, myManager.swinLimits * 2);
+
+        RaycastHit hit = new RaycastHit();//criação do raycast
+        Vector3 direction = myManager.transform.position - transform.position;
+
+
+
+        //limite que o peixe pode chegar - chegando no limite ele rotacioana circularmente e retorna ao ponto inicial
         {
             if (!b.Contains(transform.position))
             {
                 turing = true;
+                direction = myManager.transform.position - transform.position;
             }
-            else
+
+
+
+            else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))//responsavel por evitar colisão
             {
-                turing = false;
+                turing = false; // se ele nao etiver no limite significa que ainda esta no cardume
+                direction = Vector3.Reflect(this.transform.forward, hit.normal);
             }
-            if (turing)
+            else turing = false;
+
+            if (turing)// rotate 
             {
-                Vector3 direction = myManager.transform.position - transform.position;
+
                 transform.rotation = Quaternion.Slerp(transform.rotation,
                     Quaternion.LookRotation(direction),
                     myManager.rotationSpeed * Time.deltaTime);
+
+
+
+
+
+
+               
             }
             else
             {
@@ -43,7 +67,7 @@ public class Flock : MonoBehaviour
         }
 
 
-        transform.Translate(0, 0, Time.deltaTime * speed);
+        transform.Translate(0, 0, Time.deltaTime * speed);//set de velocidade
 
     }
 
@@ -53,18 +77,26 @@ public class Flock : MonoBehaviour
     
 
 
-    void ApplyRules()
+    void ApplyRules()//regras para criaçao dos flocks
     {
-        GameObject[] gos;
+        GameObject[] gos;//array dos peixes a serem criados
         gos = myManager.allFish;
-        bool turing = false;
+       // bool turing = false;
 
-        Vector3 vcentre = Vector3.zero;
-        Vector3 vavoid = Vector3.zero;
-        float gSpeed = 0.01f;
-        float nDistance;
-        int groupSize = 0;
-        foreach (GameObject go in gos)
+        Vector3 vcentre = Vector3.zero;//calculo de centralização
+        Vector3 vavoid = Vector3.zero;//refernca ponto central
+        float gSpeed = 0.01f;// velocidade inicial para equlibrar 
+        float nDistance;//calculo de distancia entre um objeto e outro
+        int groupSize = 0;//quantidade de grupos de cardumes -
+
+
+
+
+        foreach (GameObject go in gos)/*criando objetos peixes,calculando distancia entre eles,
+            assim em seguida faz o calculo central e finaliza calculando a quantidade 
+
+
+            */
         {
             if (go != this.gameObject)
             {
@@ -82,16 +114,18 @@ public class Flock : MonoBehaviour
                             {
                                 vavoid = vavoid + (this.transform.position - go.transform.position);
                             }
-
+                            //pegado do metododo da classe flock-
                             Flock anotherFlock = go.GetComponent<Flock>();
-                            gSpeed = gSpeed + anotherFlock.speed;
+                            gSpeed = gSpeed + anotherFlock.speed;// reprograma quando o objeto esta muito rapido assim cria uma desaaceleração.
                }
                     
             }
         }
-        if (groupSize > 0)
+        if (groupSize > 0)//configuração dentro do grupo 
         { vcentre = vcentre / groupSize + (myManager.goalPos - this.transform.position);
             speed = gSpeed / groupSize;
+
+
             Vector3 direction = (vcentre + vavoid) - transform.position;
             if (direction != Vector3.zero)
                 transform.rotation = Quaternion.Slerp(transform.rotation,
--- Assets/FlockManager.cs
+++ Assets/FlockManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
-    public GameObject fishPrefab;
+    public GameObject fishPrefab;//diretiva do objeto peixe
     public int numFish = 20;
     public GameObject[] allFish;
     public Vector3 swinLimits = new Vector3(5, 5, 5);
     public Vector3 goalPos;
-    [Header("Configurações do Cardume")]
+    [Header("Configurações do Cardume")] //menu de gerenciamento do peixe
 
     [Range(0.0f, 5.0f)]
     public float minSpeed;
@@ -18,20 +18,20 @@ public class FlockManager : MonoBehaviour
     [Range(1.0f, 10.0f)]
     public float neighbourDistance;
     [Range(0.0f, 5.0f)]
-    public float rotationSpeed;
+    public float rotationSpeed;//variavel de rotação
 
 
     void Start()
     {
-        allFish = new GameObject[numFish];
+        allFish = new GameObject[numFish];//criação dos peixe perante as setagem
         for (int i = 0; i < numFish; i++)
         {
             Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
               Random.Range(-swinLimits.y, swinLimits.y),
               Random.Range(-swinLimits.z, swinLimits.z));
 
-            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
-            allFish[i].GetComponent<Flock>().myManager = this;
+            allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity); //instancia o prefab
+            allFish[i].GetComponent<Flock>().myManager = this;// pegada do metodo dentro do flock
         }
         goalPos = this.transform.position;
     }
